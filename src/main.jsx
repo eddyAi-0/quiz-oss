4810dc40ad5e1a2f96d7d6a5b1398d7ccbbc7c7f
eddyAi-0 <epolice44@gmail.com>
2026-05-22 13:10:39 +0200

    refactor: split index.css in 5 moduli (base, components, quiz, dashboard, chat)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import './styles/base.css'
+import './styles/components.css'
+import './styles/quiz.css'
+import './styles/dashboard.css'
+import './styles/chat.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
